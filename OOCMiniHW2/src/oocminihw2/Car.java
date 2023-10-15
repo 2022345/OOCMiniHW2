240398a66b2ce13c50784d749c4b23f9a3594b7a
2022345 <2022345@student.cct.ie>
2023-10-15 11:55:29 +0100

    Update Car.java
    Removed the Constructor from class Car

--- OOCMiniHW2/src/oocminihw2/Car.java
+++ OOCMiniHW2/src/oocminihw2/Car.java
@@ -10,9 +10,8 @@ package oocminihw2;
  */
 public class Car extends Vehicle {
     
-    public Car (float speed, float direction, String make, String type, int numwheels, int numPassengers){
-        this.speed = speed;
-        this.direction = direction;
+    public Car ( String make, String type, int numwheels, int numPassengers){
+        
         this.make = make;
         this.type = type;
         this.numWheels = 4;
@@ -20,44 +19,8 @@ public class Car extends Vehicle {
         
     }
 
-    public float getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(float speed) {
-        this.speed = speed;
-    }
-
-    public float getDirection() {
-        return direction;
-    }
-
-    public void setDirection(float direction) {
-        this.direction = direction;
-    }
-
-    public String getMake() {
-        return make;
-    }
-
-    public void setMake(String make) {
-        this.make = make;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public int getNumPassengers() {
-        return numPassengers;
-    }
-
-    public void setNumPassengers(int numPassengers) {
-        this.numPassengers = numPassengers;
+    Car() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
