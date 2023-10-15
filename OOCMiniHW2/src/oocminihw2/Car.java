2c9c62caafe89697b3937e62dd6044bb5c1acdef
2022345 <2022345@student.cct.ie>
2023-10-15 10:28:21 +0100

    Create Car.java
    Created a subclass to Vehicle called Car.

---
+++ OOCMiniHW2/src/oocminihw2/Car.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oocminihw2;
+
+/**
+ *
+ * @author suele
+ */
+public class Car extends Vehicle {
+    
+    public Car (float speed, float direction, String make, String type, int numwheels, int numPassengers){
+        this.speed = speed;
+        this.direction = direction;
+        this.make = make;
+        this.type = type;
+        this.numWheels = 4;
+        this.numPassengers = 5;
+        
+    }
+    
+}
